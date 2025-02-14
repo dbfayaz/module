07c03a439e8b30b55d87fed7e49b6b8c84b67fa9
dbfayaz <fayazdb2020@gmail.com>
2025-02-14 09:59:58 +0530

    Add files via upload

--- variable.tf
+++ variable.tf
@@ -1,45 +1,14 @@
-variable "subscription_id" {
-  description = "the azure subscription id given here"
-  type = string
-}
-variable "resource_group_name" {
-  description = "The name of the Resource Group where all resources will be deployed."
+variable "adf_name" {
+  description = "The name of the Azure Data Factory instance."
   type        = string
 }
 
 variable "location" {
-  description = "The Azure region where resources will be created (e.g., 'East US')."
+  description = "The Azure region where the Data Factory instance will be deployed."
   type        = string
 }
 
-variable "tags" {
-  description = "A map of tags to assign to resources."
-  type        = map(string)
-  default     = {}
-}
-
-variable "vnet_name" {
-  description = "The name of the Virtual Network."
+variable "resource_group_name" {
+  description = "The name of the resource group that will host the Data Factory instance."
   type        = string
-}
-
-variable "base_appname" {
-  description = "The base application name used to form resource names."
-  type        = string
-}
-
-variable "env_name" {
-  description = "The environment identifier (e.g., 'dev', 'prod') used to form resource names."
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "The tenant ID for your Azure subscription."
-  type        = string
-}
-
-variable "object_id" {
-  description = "The object ID of the service principal or managed identity (if applicable)."
-  type        = string
-  default     = ""
 }
